0fd82768e2972b8b60cefaac40888477281d4dc1
Unidade <dario.rodri123@gmail.com>
2026-01-08 22:29:43 -0300

    Squashed 'avatar-readyplayerme-lipsycn-example-react/' content from commit f959158
    git-subtree-dir: avatar-readyplayerme-lipsycn-example-react
    git-subtree-split: f959158b2dfdd48ab4ed7e8b0e47e995f53f10d4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AvatarView from './Screen/AvatarView'
+import { Leva } from "leva";
+
+const App = () => {
+  return (
+    <div>
+      <Leva hidden />
+      <AvatarView />
+    </div>
+  )
+}
+
+export default App
